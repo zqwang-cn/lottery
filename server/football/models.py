457da3f4c5dbf6dab7793a3916183bc65ca3720d
zqwang-cn <zqwang.cn@gmail.com>
2015-07-31 12:15:20 +0800

    Modify models

--- server/football/models.py
+++ server/football/models.py
@@ -7,7 +7,7 @@ class Match(models.Model):
     away=models.CharField(max_length=20)
     handicap=models.BooleanField()
     odds=models.CharField(max_length=500)
-    results=models.CharField(max_length=20)
+    results=models.CharField(max_length=5)
 
     def get_odds():
         pass
@@ -18,23 +18,32 @@ class Match(models.Model):
     def set_results():
         pass
 
+class CurrentMatch(models.Model):
+    cmatch=models.ForeignKey(Match)
+
 class FootballBill(models.Model):
     acct=models.ForeignKey(Account)
-    type=models.PositiveSmallIntegerField()
+    #type=models.PositiveSmallIntegerField()
     comb_type=models.PositiveSmallIntegerField()
-    bet_count=models.PositiveSmallIntegerField()
+    bet_count=models.PositiveIntegerField()
+    match_count=models.PositiveSmallIntegerField()
+    finished_match_count=models.PositiveSmallIntegerField(default=0)
     times=models.PositiveSmallIntegerField(default=1)
     is_payed=models.BooleanField(default=0)
     bonus=models.DecimalField(max_digits=10,decimal_places=2,default=0.0)
-    finished=models.BooleanField(default=0)
 
-class FootballBet(models.Model):
+class FootballBillDetail(models.Model):
     bill=models.ForeignKey(FootballBill)
-    type=models.PositiveSmallIntegerField()
-    won=models.NullBooleanField()
-    bonus=models.DecimalField(max_digits=10,decimal_places=2,default=0.0)
-
-class FootballBetDetail(models.Model):
-    bet=models.ForeignKey(FootballBet)
     match=models.ForeignKey(Match)
-    cotent=models.PositiveSmallIntegerField()
+    content=models.CharField(max_length=100)
+
+#class FootballBet(models.Model):
+#    bill=models.ForeignKey(FootballBill)
+#    type=models.PositiveSmallIntegerField()
+#    won=models.NullBooleanField()
+#    bonus=models.DecimalField(max_digits=10,decimal_places=2,default=0.0)
+#
+#class FootballBetDetail(models.Model):
+#    bet=models.ForeignKey(FootballBet)
+#    match=models.ForeignKey(Match)
+#    content=models.PositiveSmallIntegerField()
