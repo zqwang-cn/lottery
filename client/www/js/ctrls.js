e492cc13856fae84a25c0ed3c4cdecf5a779b3d0
zqwang-cn <zqwang.cn@gmail.com>
2015-09-01 17:26:20 +0800

    update

--- client/www/js/ctrls.js
+++ client/www/js/ctrls.js
@@ -1,4 +1,4 @@
-var server="http://127.0.0.1:8000";
+var server="http://192.168.1.250:8000";
 var acct;
 //acct.email='a@b.com';
 //acct.password='qwe';
@@ -15,7 +15,7 @@ var optionText=['胜','平','负','让球胜','让球平','让球负','0','1','2
               '0:1','0:2','1:2','0:3','1:3','2:3','0:4','1:4','2:4','0:5','1:5','2:5','负其它'];
 
 function options2text(options){
-    texts=[];
+    var texts=[];
     var i;
     for(i=0;i<options.length;i++){
         texts.push(optionText[options[i]]);
@@ -24,7 +24,7 @@ function options2text(options){
 }
 
 function combs2text(combs){
-    texts=[];
+    var texts=[];
     var i;
     for(i=0;i<combs.length;i++){
         texts.push(combs[i]+'串1');
@@ -40,19 +40,31 @@ function myhttp($http,url,data,successfunc){
         headers: {'Content-Type':'text/plain'}
     })
     .success(function(data){
-        if(data['errmsg']=='success'){
+        if(data.errmsg=='success'){
             successfunc(data);
         }
-        else
-            alert(data['errmsg']);
+        else{
+            alert(data.errmsg);
+        }
     })
     .error(function(data){
         alert('http error');
     });
 }
 
-app.controller('MenuCtrl',['$scope','$state',
-    function($scope,$state){
+app.controller('MenuCtrl',['$scope','$state','$ionicPopup',
+    function($scope,$state,$ionicPopup){
+        $scope.isSignedIn=function(){
+            return acct!==undefined;
+        };
+        $scope.signout=function(){
+            $ionicPopup.confirm({
+                title: '确认注销',
+                template: '是否确认注销'
+            }).then(function(yes){
+                acct=undefined;
+            });
+        };
         $scope.go=function(next,params){
             if(acct===undefined){
                 $state.go('signin');
@@ -96,8 +108,8 @@ app.controller('MenuCtrl',['$scope','$state',
         //};
     }]);
 
-app.controller('SigninCtrl',['$scope','$http','$cordovaToast','$state','$ionicHistory',
-    function($scope,$http,$cordovaToast,$state,$ionicHistory){
+app.controller('SigninCtrl',['$scope','$http','$state','$ionicHistory',
+    function($scope,$http,$state,$ionicHistory){
         $scope.signinData = {};
         $scope.signin=function(){
             data=myhttp($http,server+'/account/signin',$scope.signinData,function(data){
@@ -110,8 +122,8 @@ app.controller('SigninCtrl',['$scope','$http','$cordovaToast','$state','$ionicHi
         };
     }]);
 
-app.controller('SignupCtrl',['$scope','$http','$cordovaToast','$state','$ionicHistory',
-    function($scope,$http,$cordovaToast,$state,$ionicHistory){
+app.controller('SignupCtrl',['$scope','$http','$ionicHistory',
+    function($scope,$http,$ionicHistory){
         $scope.signupData = {sex:"0"};
         $scope.signup=function(){
             myhttp($http,server+'/account/signup',$scope.signupData,function(data){
@@ -124,8 +136,8 @@ app.controller('SignupCtrl',['$scope','$http','$cordovaToast','$state','$ionicHi
         };
     }]);
 
-app.controller('AccountDetailCtrl',['$scope','$state','$ionicHistory',
-    function($scope,$state,$ionicHistory){
+app.controller('AccountDetailCtrl',['$scope',
+    function($scope){
         if(acct===undefined){
             alert('no account');
         }
@@ -136,10 +148,11 @@ app.controller('AccountDetailCtrl',['$scope','$state','$ionicHistory',
 
 app.controller('FootballCtrl',['$scope','$state','$http','$ionicModal',
     function($scope,$state,$http,$ionicModal){
-        //if(acct==undefined){
-        //    alert('no account');
-        //    return;
-        //}
+        if(acct===undefined){
+            alert('no account');
+            return;
+        }
+
         $scope.categories=[
             {title:'胜平负',indices:[[0,1,2]]},
             {title:'让球胜平负',indices:[[3,4,5]]},
@@ -156,10 +169,11 @@ app.controller('FootballCtrl',['$scope','$state','$http','$ionicModal',
         $scope.optionSelected=[];
         $scope.showOptions=function(index){
             $scope.match=$scope.matches[index];
+            var i;
             for(i=0;i<54;i++){
                 $scope.optionSelected[i]=false;
             }
-            selectedOptions=$scope.match.selectedOptions;
+            var selectedOptions=$scope.match.selectedOptions;
             if(selectedOptions!==undefined){
                 for(i=0;i<selectedOptions.length;i++){
                     $scope.optionSelected[selectedOptions[i]]=true;
@@ -171,7 +185,8 @@ app.controller('FootballCtrl',['$scope','$state','$http','$ionicModal',
             $scope.optionsModal.hide();
         };
         $scope.confirmOptions=function(){
-            selectedOptions=[];
+            var selectedOptions=[];
+            var i;
             for(i=0;i<54;i++){
                 if($scope.optionSelected[i]){
                     selectedOptions.push(i);
@@ -203,7 +218,7 @@ app.controller('FootballCtrl',['$scope','$state','$http','$ionicModal',
         };
         $scope.doRefresh=function(){
             myhttp($http,server+'/football/getMatchInfo',{email:acct.email,password:acct.password},function(data){
-                $scope.matches=data['matches'];
+                $scope.matches=data.matches;
                 $scope.$broadcast('scroll.refreshComplete');
             });
         };
@@ -213,6 +228,7 @@ app.controller('FootballCtrl',['$scope','$state','$http','$ionicModal',
 app.controller('FootballConfirmCtrl',['$scope','$state','$http','$ionicModal','$ionicHistory','$ionicPopup',
     function($scope,$state,$http,$ionicModal,$ionicHistory,$ionicPopup){
         $scope.selectedMatches=[];
+        var i;
         for(i=0;i<matches.length;i++){
             match=matches[i];
             if(match.selectedOptions!==undefined){
@@ -231,6 +247,7 @@ app.controller('FootballConfirmCtrl',['$scope','$state','$http','$ionicModal','$
                 $scope.multiple-=1;
             }
         };
+
         $ionicModal.fromTemplateUrl('html/comb.html',{
             scope:$scope
         }).then(function(modal){
@@ -270,11 +287,17 @@ app.controller('FootballConfirmCtrl',['$scope','$state','$http','$ionicModal','$
                 }
             }
             if(selectedCombs.length===0){
-                alert('no comb selected');
+                $ionicPopup.alert({
+                    title: '错误',
+                    template: '请选择串关方式！'
+                });
                 return;
             }
             if(!$scope.multiple||$scope.multiple<1||$scope.multiple>99){
-                alert("multiple:1-99");
+                $ionicPopup.alert({
+                    title: '错误',
+                    template: "倍数必须在1－99之间！" 
+                });
                 return;
             }
 
@@ -291,6 +314,7 @@ app.controller('FootballConfirmCtrl',['$scope','$state','$http','$ionicModal','$
                 billInfo.multiple=$scope.multiple;
                 billInfo.combs=selectedCombs;
                 billInfo.matches=[];
+                var i;
                 for(i=0;i<$scope.selectedMatches.length;i++){
                     match=$scope.selectedMatches[i];
                     billInfo.matches.push({
@@ -300,8 +324,8 @@ app.controller('FootballConfirmCtrl',['$scope','$state','$http','$ionicModal','$
                 }
 
                 myhttp($http,server+'/football/createBill',billInfo,function(data){
-                    backBackViewId=$ionicHistory.backView()['backViewId'];
-                    backBackView=$ionicHistory.viewHistory()['views'][backBackViewId];
+                    backBackViewId=$ionicHistory.backView().backViewId;
+                    backBackView=$ionicHistory.viewHistory().views[backBackViewId];
                     $ionicHistory.currentView(backBackView);
                     $ionicHistory.clearCache();
                     $state.go('footballBillDetail',{billid:data.billid},{location:'replace'});
@@ -314,9 +338,6 @@ app.controller('FootballBillsCtrl',['$scope','$state','$http',
     function($scope,$state,$http){
         myhttp($http,server+'/football/getFootballBills',{email:acct.email,password:acct.password},function(data){
             $scope.fbills=data.bills.reverse();
-            //if($state.params['forward']=='true'){
-            //    $state.go('footballBillDetail',{billid:$scope.fbills[0].id});
-            //}
         });
         $scope.showDetail=function(index){
             $state.go('footballBillDetail',{billid:$scope.fbills[index].id});
@@ -325,26 +346,27 @@ app.controller('FootballBillsCtrl',['$scope','$state','$http',
 
 app.controller('FootballBillDetailCtrl',['$scope','$state','$ionicPopup','$http','$ionicHistory',
     function($scope,$state,$ionicPopup,$http,$ionicHistory){
-        billid=$state.params['billid'];
+        var billid=$state.params.billid;
         myhttp($http,server+'/football/getFootballBillDetail',{email:acct.email,password:acct.password,billid:billid},function(data){
             $scope.fbill=data.bill;
+            var i;
             for(i=0;i<$scope.fbill.matches.length;i++){
-                options=JSON.parse($scope.fbill.matches[i].selectedOptions);
+                var options=JSON.parse($scope.fbill.matches[i].selectedOptions);
                 $scope.fbill.matches[i].selectedOptionsText=options2text(options);
             }
-            combs=JSON.parse($scope.fbill.comb_type);
+            var combs=JSON.parse($scope.fbill.comb_type);
             $scope.fbill.combsText=combs2text(combs);
         });
 
         $scope.pay=function(){
-            money=2*$scope.fbill.bet_count*$scope.fbill.multiple;
+            var money=2*$scope.fbill.bet_count*$scope.fbill.multiple;
             $ionicPopup.confirm({
                 title: '确认付款',
                 template: '是否确认付款'+money+'元？'
             }).then(function(yes){
                 if(yes){
-                    billid=$scope.fbill.id;
-                    payInfo={};
+                    var billid=$scope.fbill.id;
+                    var payInfo={};
                     payInfo.billid=billid;
                     payInfo.email=acct.email;
                     payInfo.password=acct.password;
@@ -387,6 +409,7 @@ app.controller('TraditionalCtrl',['$scope','$state','$ionicPopup','$http','$ioni
         $scope.traditional_info={};
         $scope.traditional_info.type=$state.params.type;
         $scope.selected=[];
+        var i;
         for(i=0;i<14;i++){
             $scope.selected.push([false,false,false]);
         }
@@ -460,7 +483,7 @@ app.controller('TraditionalConfirmCtrl',['$scope','$state','$ionicPopup','$http'
                 if(!yes){
                     return;
                 }
-                billInfo={};
+                var billInfo={};
                 billInfo.email=acct.email;
                 billInfo.password=acct.password;
                 billInfo.id=$scope.traditional_info.id;
@@ -469,7 +492,7 @@ app.controller('TraditionalConfirmCtrl',['$scope','$state','$ionicPopup','$http'
                 billInfo.matches=[];
                 var i;
                 for(i=0;i<traditional_info.matches.length;i++){
-                    match=traditional_info.matches[i];
+                    var match=traditional_info.matches[i];
                     billInfo.matches.push(match.selectedOptions);
                 }
 
@@ -478,8 +501,8 @@ app.controller('TraditionalConfirmCtrl',['$scope','$state','$ionicPopup','$http'
                         title:'下单成功',
                         template:'下单成功！'
                     }).then(function(){
-                        backBackViewId=$ionicHistory.backView()['backViewId'];
-                        backBackView=$ionicHistory.viewHistory()['views'][backBackViewId];
+                        backBackViewId=$ionicHistory.backView().backViewId;
+                        backBackView=$ionicHistory.viewHistory().views[backBackViewId];
                         $ionicHistory.currentView(backBackView);
                         $ionicHistory.clearCache();
                         $state.go('traditionalBills',{forward:true},{location:'replace'});
@@ -503,20 +526,20 @@ app.controller('TraditionalBillsCtrl',['$scope','$state','$http',
 app.controller('TraditionalBillDetailCtrl',['$scope','$state','$ionicPopup','$http','$ionicHistory',
     function($scope,$state,$ionicPopup,$http,$ionicHistory){
         $scope.options2text=options2text;
-        billid=$state.params.billid;
+        var billid=$state.params.billid;
         myhttp($http,server+'/football/getTraditionalBillDetail',{email:acct.email,password:acct.password,billid:billid},function(data){
             $scope.bill=data.bill;
         });
 
         $scope.pay=function(){
-            money=2*$scope.bill.bet_count*$scope.bill.multiple;
+            var money=2*$scope.bill.bet_count*$scope.bill.multiple;
             $ionicPopup.confirm({
                 title: '确认付款',
                 template: '是否确认付款'+money+'元？'
             }).then(function(yes){
                 if(yes){
-                    billid=$scope.bill.id;
-                    payInfo={};
+                    var billid=$scope.bill.id;
+                    var payInfo={};
                     payInfo.billid=billid;
                     payInfo.email=acct.email;
                     payInfo.password=acct.password;
@@ -537,7 +560,7 @@ app.controller('TraditionalBillDetailCtrl',['$scope','$state','$ionicPopup','$ht
                 template: '是否确认删除订单？'
             }).then(function(yes){
                 if(yes){
-                    delInfo={};
+                    var delInfo={};
                     delInfo.email=acct.email;
                     delInfo.password=acct.password;
                     delInfo.billid=$scope.bill.id;
